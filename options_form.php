918edc3dd9e8c8d8124c890d4b99b3ccb1106879
kranthitech <kranthitech@gmail.com>
2015-10-03 22:18:24 +0530

    added variable inside script

--- options_form.php
+++ options_form.php
@@ -1,12 +1,12 @@
-
+<script>var tupuk_sample_plugin_value = <?php echo get_option( 'extra_post_info' ); ?></script>
 <form method="post" action="options.php">
   <?php settings_fields( 'extra-post-info-settings' ); ?>
   <?php do_settings_sections( 'extra-post-info-settings' ); ?>
 
-  <table class="form-table">
+  <table class="form-table" style="display:none" >
     <tr valign="top">
-    <th scope="row">Extra post info:</th>
-    <td><input style="display:none" type="text" name="extra_post_info" value="<?php echo get_option( 'extra_post_info' ); ?>"/></td>
+    <th scope="row">Options Field</th>
+    <td><input type="text" name="extra_post_info" value="<?php echo get_option( 'extra_post_info' ); ?>"/></td>
     </tr>
   </table>
   <?php submit_button(); ?>
