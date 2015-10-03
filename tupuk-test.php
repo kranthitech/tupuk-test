42041a13c58976063d17a733ecfdb8abeafaa708
kranthitech <kranthitech@gmail.com>
2015-10-03 18:15:14 +0530

    added register function

--- tupuk-test.php
+++ tupuk-test.php
@@ -18,23 +18,19 @@ function blog_page_code(){
 
 function plugin_admin_add_page() {
 	add_options_page('Tupuk Settings Page', 'Tupuk Plugin Menu', 'manage_options', 'tupuk_settings', 'plugin_options_page');
+
+	add_action( 'admin_init', 'register_mysettings' ); //call register settings function
 }
 
+
+function register_mysettings() {
+	register_setting( 'extra-post-info-settings', 'extra_post_info' );
+ }
+
+// Add settings link on plugin page
 $plugin = plugin_basename(__FILE__); 
 add_filter("plugin_action_links_$plugin", 'your_plugin_settings_link' );
 
-
-
-function my_plugin_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-	}
-	echo '<div class="wrap">';
-	echo '<p>Here is where the form would go if I actually had options.</p>';
-	echo '</div>';
-}
-
-// Add settings link on plugin page
 function your_plugin_settings_link($links) { 
   $settings_link = '<a href="options-general.php?page=tupuk_settings">Settings</a>'; 
   array_unshift($links, $settings_link); 
@@ -49,9 +45,11 @@ function plugin_options_page() {
 <div>
 <h2>My custom plugin</h2>
 Options relating to the Custom Plugin.
+
   <form method="post" action="options.php">
     <?php settings_fields( 'extra-post-info-settings' ); ?>
     <?php do_settings_sections( 'extra-post-info-settings' ); ?>
+
     <table class="form-table">
       <tr valign="top">
       <th scope="row">Extra post info:</th>
