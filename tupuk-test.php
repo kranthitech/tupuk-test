ac17dd6db53c7c5badc8d48596224c41c667f493
kranthitech <kranthitech@gmail.com>
2015-10-03 20:12:26 +0530

    added array format for all function references

--- tupuk-test.php
+++ tupuk-test.php
@@ -10,14 +10,38 @@ class TupukSamplePlugin{
 
 	function __construct() {
 		add_action( 'wp_footer', array($this, 'blog_page_code') );
-		
+		add_action( 'admin_menu', array($this, 'plugin_admin_add_page') );
+
+		// Add settings link on plugin page
+		$plugin = plugin_basename(__FILE__); 
+		add_filter("plugin_action_links_$plugin", array($this, 'your_plugin_settings_link') );
 	}
 	
 	function blog_page_code(){
 		echo '<script> console.log("TUPUK TEST");</script>';
 	}
+
+	function plugin_admin_add_page() {
+		add_options_page($widget_label+' Settings', 'Tupuk '+$widget_label, 'manage_options', 'tupuk_'+$widget_tag, array($this, 'plugin_options_page'));
+
+		add_action( 'admin_init', array($this, 'register_mysettings') ); //call register settings function
+	}
+
+	function register_mysettings() {
+		register_setting( 'extra-post-info-settings', 'extra_post_info' );
+	}
+
+	function your_plugin_settings_link($links) { 
+	  $settings_link = '<a href="options-general.php?page=tupuk_settings">Settings</a>'; 
+	  array_unshift($links, $settings_link); 
+	  return $links; 
+	}
+
+	function plugin_options_page(){
+		include dirname( __FILE__ ) . 'options_form.php';
+	}
 }
-$tupuk_sampe_plugin = new TupukSamplePlugin();
+
 
 ?>
 
