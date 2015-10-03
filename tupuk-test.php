245f4e56df94929a5a90ac73a73ebeeed4303561
kranthitech <kranthitech@gmail.com>
2015-10-03 15:40:19 +0530

    added a small javascript in blog

--- tupuk-test.php
+++ tupuk-test.php
@@ -10,7 +10,7 @@ add_action( 'wp_footer', 'blog_page_function' );
 
 
 function blog_page_function(){
-	echo '<div> SOME NEW CONTENT THROUGH THE PLUGIN</div>';
+	echo '<script> console.log("TUPUK TEST");</script>';
 }
 
 ?>
