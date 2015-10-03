1fb9648c86fe2cb0fd7adb68974865bdce52f4e3
kranthitech <kranthitech@gmail.com>
2015-10-03 21:37:39 +0530

    plugin options page outside class

--- tupuk-test.php
+++ tupuk-test.php
@@ -37,9 +37,29 @@ class TupukSamplePlugin{
 	  return $links; 
 	}
 
-	function plugin_options_page(){
-		include dirname( __FILE__ ) . 'options_form.php';
-	}
+
+}
+
+function plugin_options_page(){
+?>
+    <h2>My custom plugin</h2>
+  Options relating to the Custom Plugin.
+  Previous value - <p><?php echo get_option( 'extra_post_info' ); ?></p>
+
+  <form method="post" action="options.php">
+    <?php settings_fields( 'extra-post-info-settings' ); ?>
+    <?php do_settings_sections( 'extra-post-info-settings' ); ?>
+
+    <table class="form-table">
+      <tr valign="top">
+      <th scope="row">Extra post info:</th>
+      <td><input type="text" name="extra_post_info" value="<?php echo get_option( 'extra_post_info' ); ?>"/></td>
+      </tr>
+    </table>
+    <?php submit_button(); ?>
+  </form>
+
+<?php
 }
 
 
