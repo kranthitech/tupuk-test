d710e744e8f4ff183f8cc8d77ef2ca0f462a933d
kranthitech <kranthitech@gmail.com>
2015-10-03 15:35:09 +0530

    added Plugin Name and other details

--- tupuk-test.php
+++ tupuk-test.php
@@ -1,4 +1,11 @@
 <?php
+/*
+Plugin Name: Tupuk Test
+Description: This is a play ground to test plugin functions
+Author: Kranthi Kiran
+Version: 0.1
+*/
+
 add_action( 'wp_footer', 'blog_page_function' );
 
 
