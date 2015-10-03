578dcf905cff39b28d1e9b4c9ce2cfdbd1605c6f
kranthitech <kranthitech@gmail.com>
2015-10-03 22:14:25 +0530

    removed extra function

--- tupuk-test.php
+++ tupuk-test.php
@@ -23,7 +23,7 @@ class TupukSamplePlugin{
 
 	function plugin_admin_add_page() {
 		//add_options_page('Tupuk Sample Plugin Settings', 'Tupuk Sample Plugin', 'manage_options', 'tupuk_sample_plugin',  'plugin_options_page');
-		add_options_page('Tupuk Sample Plugin Settings', 'Tupuk Sample Plugin', 'manage_options', 'tupuk_sample_plugin',  array($this, 'plugin_external_page'));
+		add_options_page('Tupuk Sample Plugin Settings', 'Tupuk Sample Plugin', 'manage_options', 'tupuk_sample_plugin',  array($this, 'plugin_options_page'));
 		add_action( 'admin_init', array($this, 'register_mysettings') ); //call register settings function
 	}
 
@@ -37,31 +37,9 @@ class TupukSamplePlugin{
 	  return $links; 
 	}
 
-	function plugin_external_page(){
+	function plugin_options_page(){
 		?><?php include 'options_form.php';?><?php
 	}
-}
-
-function plugin_options_page(){
-?>
-    <h2>My custom plugin</h2>
-  Options relating to the Custom Plugin.
-  Previous value - <p><?php echo get_option( 'extra_post_info' ); ?></p>
-
-  <form method="post" action="options.php">
-    <?php settings_fields( 'extra-post-info-settings' ); ?>
-    <?php do_settings_sections( 'extra-post-info-settings' ); ?>
-
-    <table class="form-table">
-      <tr valign="top">
-      <th scope="row">Extra post info:</th>
-      <td><input type="text" name="extra_post_info" value="<?php echo get_option( 'extra_post_info' ); ?>"/></td>
-      </tr>
-    </table>
-    <?php submit_button(); ?>
-  </form>
-
-<?php
 }
 
 $tupuk_sample_plugin = new TupukSamplePlugin();
