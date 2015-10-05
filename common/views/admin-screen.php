c207b3c5cde4e30e4ea6f4946e0676bb3dac6533
kranthitech <kranthitech@gmail.com>
2015-10-05 18:31:29 +0530

    added container class

--- common/views/admin-screen.php
+++ common/views/admin-screen.php
@@ -7,15 +7,15 @@
 
 
 
-<div ng-app="tupukAdmin">
+<div ng-app="tupukAdmin" class="container">
   <accordion close-others="'true'">
     <accordion-group heading="Look and Feel" is-open="'true'">
       This is where the template goes
     </accordion-group>
-	<accordion-group heading="Trigger" is-open="'true'">
+	<accordion-group heading="Trigger">
       Show the trigger and targeting configuration here
     </accordion-group>  </accordion>
-    <accordion-group heading="Additional Configuration" is-open="'true'">
+    <accordion-group heading="Additional Configuration">
       This content is straight in the template.
     </accordion-group>
 </div>
