11f58ebdb136aa9e995cdcc329a726ae2c1c237f
kranthitech <kranthitech@gmail.com>
2015-10-05 18:36:40 +0530

    added closing accordion tag

--- common/views/admin-screen.php
+++ common/views/admin-screen.php
@@ -4,20 +4,21 @@
 <?php wp_enqueue_script('tupuk-angular') ?>
 <?php wp_enqueue_script('tupuk-angular-ui-bootstrap') ?>
 <?php wp_enqueue_script('tupuk-admin-core') ?>
-
+ 
 
 
 <div ng-app="tupukAdmin" class="container">
-  <accordion close-others="'true'">
-    <accordion-group heading="Look and Feel" is-open="'true'">
-      This is where the template goes
-    </accordion-group>
-	<accordion-group heading="Trigger">
-      Show the trigger and targeting configuration here
-    </accordion-group>  </accordion>
-    <accordion-group heading="Additional Configuration">
-      This content is straight in the template.
-    </accordion-group>
+	<accordion close-others="'true'">
+		<accordion-group heading="Look and Feel" is-open="'true'">
+		  This is where the template goes
+		</accordion-group>
+		<accordion-group heading="Trigger">
+		  Show the trigger and targeting configuration here
+		</accordion-group>  </accordion>
+		<accordion-group heading="Additional Configuration">
+		  This content is straight in the template.
+		</accordion-group>
+	</accordion>
 </div>
 
 <?php include 'options-form.php';?>
