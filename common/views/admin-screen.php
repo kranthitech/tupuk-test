e47ee2aba2156e901163cc753e1d2eefe115c96c
kranthitech <kranthitech@gmail.com>
2015-10-05 16:27:45 +0530

    using plugin_basename

--- common/views/admin-screen.php
+++ common/views/admin-screen.php
@@ -1,4 +1,4 @@
-<script src="<?php echo basename(__FILE__)?>/scripts/angular.min.js"></script>
+<script src="<?php echo plugin_basename(__DIR__)?>/scripts/angular.min.js"></script>
 
 <div ng-app>
   <label>Name:</label>
